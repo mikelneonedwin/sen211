c08deaab217e2f8c417ae58f8acb21c0e04f4cf3
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2024-12-31 11:32:34 +0100

    Remove unused Google sign-up code and update result object properties in firebase.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import { googleSignUp } from "./lib/firebase.js"; // Ensure the correct path and file extension
-
-document.getElementById("googleSignUpButton").addEventListener("click", () => {
-  console.log("hey");
-  googleSignUp().catch((error) => console.error(error));
-});
